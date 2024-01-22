3b1ec25ba2fd0b3eb9d9e5fe5e2048fca829062b
azinedine <amarichezineddine@gmail.com>
2024-01-22 19:21:20 +0100

    pushed

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: ['react-native-reanimated/plugin',
-  ["module:react-native-dotenv", {
-    "envName": "APP_ENV",
-    "moduleName": "@env",
-    "path": ".env",
-  }]]
-
+  presets: ['module:metro-react-native-babel-preset'],
 };
-
-
--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 /**
  * @format
  */
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
- 
 
-// AppRegistry.registerComponent(appName, () => App);
-// import { Navigation } from 'react-native-navigation';
-
-
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
-
-AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
+AppRegistry.registerComponent(appName, () => App);
